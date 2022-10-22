fb0b0f9b059697d44d7c0c073f8f7239e8253836
SaifurRahmanBijoy <saifurrahmanbijoy1@gmail.com>
2022-10-22 23:42:34 +0600

    reg

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,8 +4,8 @@ import { AuthContext } from "../../contexts/UserContext";
 const About = () => {
   const { user } = useContext(AuthContext);
   return (
-    <div className="text-center my-8 text-3xl font-serif">
-      <h2>The info about us!</h2>
+    <div className="text-center my-8 text-md font-serif text-wrap">
+      <h2>The information!</h2>
       <p>Welcome,{user?.email}</p>
     </div>
   );
