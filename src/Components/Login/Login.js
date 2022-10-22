79ec4d80d3b537fb069f2ee7f79e66283af6fb35
SaifurRahmanBijoy <saifurrahmanbijoy1@gmail.com>
2022-10-22 15:35:08 +0600

    reg

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/UserContext";
 
 const Login = () => {
+  const { signIn } = useContext(AuthContext);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+    signIn(email, password)
+      .then((res) => {
+        const user = res.user;
+        console.log(user);
+        form.reset();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <div className="form-container bg-slate-50 w-5/6 sm:w-1/3 mx-auto mt-2 border-2 border-slate-300 drop-shadow-xl rounded-sm">
       <h2 className="text-3xl text-center py-3 font-serif">Login</h2>
-      <form className="px-5">
+      <form onSubmit={handleSubmit} className="px-5">
         <div className="form-control mb-1">
           <label
             className="block mb-1 text-md font-mono pl-2 text-slate-600"
